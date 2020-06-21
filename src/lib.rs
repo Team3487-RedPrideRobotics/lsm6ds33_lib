6cc206e920cdebfc6f85f57d538597294db12e63
Lainkai <tefman7422@gmail.com>
2020-06-21 14:50:07 -0400

    FIFO not a priority

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,5 @@
 
-//FIFO Config Registers
 
-/// FIFO contol register (r/w).
-/// FIFO threshold level setting.
-/// Default value: 0000 0000.
-/// Watermark falg rises when th enumber of bytes written
-/// to FIFO after the next write is greater than or equal
-/// to the threshold level.
-/// Minimum resolution fo the FIFO is 1 LSB = 2 bytes (1 word) in FIFO
-pub const FIFO_CTRL1:u8 = 0x06;
-pub const FIFO_CTRL2:u8 = 0x07;
-pub const FIFO_CTRL3:u8 = 0x08;
-pub const FIFO_CTRL4:u8 = 0x09;
-pub const FIFO_CTRL5:u8 = 0x0A;
 
 #[cfg(test)]
 mod tests {
